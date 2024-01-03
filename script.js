88b25190d000c1a4bd2e0d97ec7965c51f8f2306
Keenan Hallas <60987718+keenanhallas@users.noreply.github.com>
2024-01-03 13:03:10 -0600

    Remove unneeded code from script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let signUpForm = document.querySelector(".sign-up-form");
 let warningSpan = document.querySelector(".password-warning");
 let submitButton = document.querySelector(".submit-btn");
 let passwordOne = document.querySelector("[name='password']");
 let passwordTwo = document.querySelector("[name='confirm-password']");
-let formInputs = document.querySelectorAll("input")
 
 function checkMatch(e) {
     if(passwordOne.value !== passwordTwo.value){
